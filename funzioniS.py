dffb91e7c5977641e9c1286aa793188f61d89107
samuelefranco16 <samuele.franco@itsrizzoli.it>
2026-02-19 14:59:05 +0100

    gggg

--- funzioniS.py
+++ funzioniS.py
@@ -24,4 +24,24 @@ def minimo(lista):
     return min
 
 def lung(lista):
-    
+    '''
+    funzione che conta gli elementi di una lista 
+    '''
+    conta = 0
+    for i in lista:
+        conta += 1
+    return conta
+
+def rango(iniz, ferma, passo):
+    while(iniz < ferma-1):
+        iniz += passo
+    return 
+
+
+
+casistita = [79,4289,990,39,755,5]
+a=4
+b=6
+c=1
+print(massimo(casistita),minimo(casistita),lung(casistita))
+rango(a,b,c)
