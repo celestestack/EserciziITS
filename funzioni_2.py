43d10cfe99bbe7802ec15b491f4137410d0a1403
samuelefranco16 <samuele.franco@itsrizzoli.it>
2026-02-19 22:04:46 +0100

    funzioni_2 complete

--- funzioni_2.py
+++ funzioni_2.py
@@ -1,4 +1,10 @@
-
+'''
+Creare un programma che chieda all’utente di inserire un numero n e stampi una stringa lunga n
+caratteri dove i caratteri che la compongono saranno i caratteri @ e # alternati partendo con un
+carattere @
+Ad esempio se l’utente dice che la stringa dovrà essere lunga 5 caratteri la stringa stampata dovrà
+essere: @#@#@
+'''
 def stringaFinale():
     num = int(input("Inserire un numero: "))
     stringa = ""
