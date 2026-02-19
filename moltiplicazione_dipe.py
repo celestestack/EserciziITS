66a118ed84b552440ffb948182b886306f772e68
celestestack <alessia.diperna@itsrizzoli.it>
2026-02-19 14:15:36 +0100

    Add initial implementation for multiplication using repeated addition

---
+++ moltiplicazione_dipe.py
@@ -0,0 +1,3 @@
+'''Creare un programma che permetta all’utente di effettuare la moltiplicazione tra due numeri senza utilizzare
+l’operatore * ma calcolando il risultato attraverso somme ripetute (numeri positivi).'''
+
