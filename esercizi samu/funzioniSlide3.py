7128a223a6c2f3305f29d65e692a0e46c73032df
FrankSamuele <francosamuele2005@gmail,com>
2026-03-04 11:19:22 +0100

    tantissime novità ragazzi

--- esercizi samu/funzioniSlide3.py
+++ esercizi samu/funzioniSlide3.py
@@ -1,3 +1,4 @@
+import math
 '''
 Riscrivete il programma ‘area.py’, definendo funzioni separate per l’area del quadrato, del rettangolo e del cerchio (3.14 *
 raggio**2). Il programma deve includere anche un’interfaccia a menu
@@ -24,11 +25,37 @@ Scrivere una funzione che produce un istogramma esempio Istrogramma([4,2,6]):
 **
 ******
 '''
+def isto(lista):
+    for i in range(len(lista)):
+        print("*"*lista[i])
 
+listas = [1,4,6,3]
+print(isto(listas))
 '''
 Scrivere una funzione che produca una lista dei numeri di Fibonacci fino ad n
 '''
+def fibo(n):
+    a = 0
+    b = 1
+    if n < 0:
+        print("Il numero deve essere positivo")
+    fibol = []
+    while a <= n:
+        fibol.append(a)
+        a, b = b, a+b
+         
+    return fibol
 
+print(fibo(2))
 '''
 Scrivere una funzione che restituisce la media dei numeri passati come lista
 '''
+def media(lista):
+    #sommare tutti gli elementi della lista e dividerli per la lunghezza della lista
+    som = 0
+    for i in range(len(lista)):
+        som += lista[i]
+    return som/len(lista)
+
+cacca = [30,29,29,22]
+print(media(cacca))
